6b7e47ca7046205922f572aaec0db1ed71a2887d
noidname <tim983649@gmail.com>
2020-12-08 09:13:07 +0800

    add port and gitignore

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 //routes
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+const port = process.env.port || 4000
+app.listen(port)
+
