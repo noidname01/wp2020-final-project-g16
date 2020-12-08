ea0b0ef9f70e56e7a4e1c2c4b6f9f315fb357f46
noidname <tim983649@gmail.com>
2020-12-08 09:17:23 +0800

    import => require

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 //routes
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
   });
 
 const port = process.env.port || 4000
