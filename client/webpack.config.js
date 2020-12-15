9e0451a8d157dad43b5d4c0f8bfcce722e849d87
noidname <tim983649@gmail.com>
2020-12-15 13:09:42 +0800

    webpack front+back

---
+++ client/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+// const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  //=============== webpack server (currently not use)=================
+  // devServer:{
+  //   contentBase: path.resolve(__dirname,'dist'),
+  //   open:true,
+  //   clientLogLevel: 'silent',
+  //   port: 9000,
+  // },
+  //===================================================================
+  //=============== html template plugin (currently not use) ==========
+  // plugins:[
+  //   new HtmlWebpackPlugin({
+  //     template: path.resolve(__dirname, "dist/index.html")
+  //   })
+  // ],
+  //===================================================================
+  module:{
+    rules: [
+      {
+        test:/\.(jsx|js)$/,
+        include: path.resolve(__dirname,'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader:'babel-loader',
+            options:{
+              presets:[
+                ['@babel/preset-env',{
+                  "targets":"defaults"
+                }],
+                '@babel/preset-react'
+              ],
+              // plugins: ["react-hot-loader/babel"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use:["style-loader","css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+          },
+        },
+      }
+    ]
+  }
+}
--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/dist')))
 
 //routes
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'))
   });
 
 const port = process.env.PORT || 5000
