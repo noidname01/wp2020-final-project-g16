c1fc61225a45ffce894b61d436e887ac8e49fb89
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-12-19 02:26:20 +0800

    add

---
+++ layout/src/containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Body from "../components/Body";
+
+function App() {
+  return (
+    <div className="editor">
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
