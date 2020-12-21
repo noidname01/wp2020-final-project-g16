ce5191b391e8c9746c6011f6f52749b8e3972421
noidname <tim983649@gmail.com>
2020-12-21 23:56:14 +0800

    add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
     <div>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,6 +51,10 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
+                test: /\.(scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: {
                     loader: 'file-loader',
