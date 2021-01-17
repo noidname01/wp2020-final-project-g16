a8bb7f06083569e0b0588f54878345106fe1f0f4
Yochi Hsu <hs41511@gmail.com>
2021-01-17 21:11:14 +0800

    upload file completed

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes/Routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ReactDataSheet from 'react-datasheet';
+import ReactDataSheet from 'react-datasheet'
 // Be sure to include styles at some point, probably during your bootstrapping
-import 'react-datasheet/lib/react-datasheet.css';
-import Excel from './containers/Excel'
+import 'react-datasheet/lib/react-datasheet.css'
 
 ReactDOM.render(
-    <div>
-        <Excel />
-    </div>,
-    document.getElementById('app')
+	<div>
+		<Routes />
+	</div>,
+	document.getElementById('app')
 )
