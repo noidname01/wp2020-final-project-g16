44fd806ff774f790989844ced169114303101dad
noidname <tim983649@gmail.com>
2021-01-17 16:33:18 +0800

    react-summernote first commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
@@ -19,6 +20,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/index.html'),
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
     ],
     //===================================================================
@@ -62,6 +67,18 @@ module.exports = {
                         name: '[path][name].[ext]',
                     },
                 },
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/',
+                        },
+                    },
+                ],
             },
         ],
     },
