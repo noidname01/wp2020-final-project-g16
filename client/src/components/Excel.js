38fd1bba11b856377b25b28a601272d0e07bcf1e
Yochi Hsu <hs41511@gmail.com>
2021-01-18 15:30:08 +0800

    Excel.js update

--- client/src/components/Excel.js
+++ client/src/components/Excel.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import ReactDataSheet from 'react-datasheet'
 import ExcelJs from 'exceljs'
 import { saveAs } from 'file-saver'
