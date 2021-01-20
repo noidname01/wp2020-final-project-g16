73699468c58625b28a9354ab426dda967dfa0598
Yochi Hsu <hs41511@gmail.com>
2021-01-21 01:19:10 +0800

    ok

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -9,6 +9,10 @@ import Template from '../containers/Template'
 import Sent from '../containers/Sent'
 import Settings from '../containers/Settings'
 
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
+
 const routes = [
     { path: '/ee/new', Component: New },
     { path: '/ee/draft', Component: Draft },
@@ -27,7 +31,7 @@ const Routes = (props) => {
             <Route path='/ee/settings' component={Settings}></Route> */}
             {routes.map(({ path, Component }) => {
                 return (
-                    <Route exact path={path}>
+                    <Route exact path={path} history={history}>
                         <Component userInfo={props.userinfo} />
                     </Route>
                 )
